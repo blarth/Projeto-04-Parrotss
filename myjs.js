837d27bb7fa9461f152931d39bda28a1f2cff0d7
blarth <jminocente@gmail.com>
2021-11-04 20:26:55 -0300

    tentando ainda a logica de entrar no jogo e agora as cartas estao virando

--- myjs.js
+++ myjs.js
@@ -1,39 +1,38 @@
 /* Comeco do jogo, checar parametros */
 function pegar_numero_cartas() {
-  let numero_cartas = parseInt(prompt("Quantos cartas deseja no jogo ?"));
-  if (numero_cartas !== NaN) {
-    return numero_cartas;
+  let resposta = parseInt(
+    prompt("Quantos cartas deseja no jogo ?(Insira um valor de 4 até 14)")
+  );
+  if (resposta !== NaN) {
+    return resposta;
   } else {
     pegar_numero_cartas();
   }
 }
-let numero_cartas = pegar_numero_cartas();
 
+var numero_cartas = pegar_numero_cartas();
+console.log(numero_cartas);
 /* Verificacao dos parametros do jogo */
-const verificacao_loop = numero_cartas % 2 !== 0;
+var verificacao_par = numero_cartas % 2 == 0;
 
 function verificacao_intervalo(x, min, max) {
   return x >= min && x <= max;
 }
 console.log(numero_cartas);
 /* loop para verificar a resposta e pedir outra se necessaria */
-
-while (
-  verificacao_intervalo(numero_cartas, 4, 14) !== true &&
-  verificacao_loop
-) {
+while (verificacao_intervalo(numero_cartas, 4, 14) !== true) {
   numero_cartas = pegar_numero_cartas();
+  console.log(numero_cartas);
 }
-
-/* 
-
-if (verificacao_intervalo(numero_cartas, 4, 14) !== true) {
-  numero_cartas = pegar_numero_cartas();
-} else {
-  if (verificacao_loop) {
-    numero_cartas = pegar_numero_cartas();
+/* function loop() {
+  if (numero_cartas < 14) {
+    if (verificacao_par !== true) {
+      numero_cartas = pegar_numero_cartas();
+      loop();
+    } else {
+    }
   } else {
-    console.log("ta certu");
+    numero_cartas = pegar_numero_cartas();
+    loop();
   }
-}
- */
+} */
